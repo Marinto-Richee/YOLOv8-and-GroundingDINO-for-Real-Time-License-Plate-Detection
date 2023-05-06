c4c9b7fb953571359ed8ffdfe5e5a11980f11f66
Marinto Richee <65499285+Marinto-Richee@users.noreply.github.com>
2023-05-06 16:29:17 +0530

    Major Update with License Detection

--- Vehicle_detection.py
+++ Vehicle_detection.py
@@ -6,12 +6,19 @@ import numpy as np
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
+#videopath="assets\\CCTV footage.mp4"
+#videopath="assets\\main gate 2.mkv"
+#videopath="assets\\CCTV Main Gate.mp4"
+#videopath="assets\\Road traffic video for object recognition.mp4"
+#videopath="assets\\main gate 1.mkv"
+#videopath="assets\\SimTrim_Trim.mp4"
+#videopath="assets\\vid2.mp4"
 videopath="assets\\video3.mp4"
 outdir="output/"
 vi=sv.VideoInfo.from_video_path(videopath)
 tracked=set()
-LINE_START = sv.Point(0, vi.height//2+150)
-LINE_END = sv.Point(vi.width, vi.height//2+150)
+LINE_END = sv.Point(0, vi.height//2+150)
+LINE_START = sv.Point(vi.width, vi.height//2+150)
 
 def main():
     line_counter = sv.LineZone(start=LINE_START, end=LINE_END)
@@ -21,8 +28,8 @@ def main():
         text_thickness=1,
         text_scale=0.5
     )
-    model = YOLO("yolov8m.pt")
-    for result in model.track(source=videopath, stream=True):
+    model = YOLO("yolov8n.pt")
+    for result in model.track(source=videopath, stream=True,device=0):
         
         frame = result.orig_img
         detections = sv.Detections.from_yolov8(result)
@@ -31,16 +38,16 @@ def main():
             detections.tracker_id = result.boxes.id.cpu().numpy().astype(int)
         
         detections = detections[(detections.class_id == 7)|(detections.class_id == 2)|(detections.class_id == 3)]
-
         labels = [
             f"{tracker_id} {model.model.names[class_id]} {confidence:0.2f}"
-            for _, confidence, class_id, tracker_id
+            for __, _, confidence, class_id, tracker_id
             in detections
         ]
 
         for box,cls_id in zip(result.boxes,detections.class_id):
             if box.cls.cpu().numpy().astype(int) in [7,2,3]:   
-                tracker_id=box.id.cpu().numpy().astype(int)
+                if box is not None:    
+                    tracker_id=box.id.cpu().numpy().astype(int)
                 x,y,x1,y1=(box.xyxy.numpy().astype(int))[0]
                 if x<0:
                     x=0
@@ -52,21 +59,25 @@ def main():
                     cv2.imwrite(path,frame[y:y1,x:x1])
                 else:
                     img_old=cv2.imread(path)
-                    area_old=img_old.shape[0]
-                    area_new=(y1-y)
+                    #area_old=img_old.shape[0]
+                    #area_new=(y1-y)
+                    area_old=img_old.shape[1]
+                    area_new=(x1-x)
                     if area_new>=area_old:
                         cv2.imwrite(path,frame[y:y1,x:x1])
+"""         cv2.imshow("yolov8", np.zeros((720,720)))
+        if (cv2.waitKey(1) == 27):
+            break
         frame = box_annotator.annotate(
             scene=frame, 
             detections=detections,
             labels=labels
         )
         line_counter.trigger(detections=detections)
-        line_annotator.annotate(frame=frame, line_counter=line_counter)
-        cv2.imshow("yolov8", frame)
-        if (cv2.waitKey(1) == 27):
-            break
-
-
+        line_annotator.annotate(frame=frame, line_counter=line_counter)"""
 if __name__ == "__main__":
     main()
+    import subprocess
+
+    # Run a Python file
+    subprocess.run(["python", "License_Plate_detection.py"])
