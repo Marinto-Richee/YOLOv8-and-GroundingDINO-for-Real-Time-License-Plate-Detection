87ee9d314a2eb440aa61a4630088ee4e1e27588b
Marinto Richee <65499285+Marinto-Richee@users.noreply.github.com>
2023-05-10 09:49:01 +0530

    Folder Structure Update

--- License_Plate_detection.py
+++ License_Plate_detection.py
@@ -19,11 +19,17 @@ from torchvision.ops import box_convert
 
 
 import os
+
 WEIGHTS_NAME = "groundingdino_swint_ogc.pth"
-if os.path.isfile(WEIGHTS_NAME) is False:    
-    get_ipython().system('wget -q https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth')
-if os.path.isfile("GroundingDINO\groundingdino\config\GroundingDINO_SwinT_OGC.py") is False:
-    get_ipython().system('git clone https://github.com/IDEA-Research/GroundingDINO.git')
+if os.path.isfile(WEIGHTS_NAME) is False:
+    get_ipython().system(
+        "wget -q https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth"
+    )
+if (
+    os.path.isfile("GroundingDINO\groundingdino\config\GroundingDINO_SwinT_OGC.py")
+    is False
+):
+    get_ipython().system("git clone https://github.com/IDEA-Research/GroundingDINO.git")
 
 
 # In[15]:
@@ -36,41 +42,48 @@ os.path.isfile("GroundingDINO\groundingdino\config\GroundingDINO_SwinT_OGC.py")
 
 
 from groundingdino.util.inference import load_model, load_image, predict, annotate
-model = load_model("GroundingDINO\groundingdino\config\GroundingDINO_SwinT_OGC.py", "groundingdino_swint_ogc.pth")
+
+model = load_model(
+    "GroundingDINO\groundingdino\config\GroundingDINO_SwinT_OGC.py",
+    "groundingdino_swint_ogc.pth",
+)
 
 
 # In[17]:
-IMAGE_FOLDER = 'output'
-LICENSE_PLATE_FOLDER = 'License Plate'
+IMAGE_FOLDER = "output"
+LICENSE_PLATE_FOLDER = "License Plate"
 TEXT_PROMPT = "Number Plate"
-BOX_THRESHOLD = 0.35
+BOX_THRESHOLD = 0.5
 TEXT_THRESHOLD = 0.3
 for file_name in os.listdir(IMAGE_FOLDER):
-    if file_name.endswith('.jpg'):
+    if file_name.endswith(".jpg"):
         # load image
         image_path = os.path.join(IMAGE_FOLDER, file_name)
         image_source, image = load_image(image_path)
         # detect license plates
-        boxes, logits, phrases = predict(
-            model=model, 
-            image=image, 
-            caption=TEXT_PROMPT, 
-            box_threshold=BOX_THRESHOLD, 
-            text_threshold=TEXT_THRESHOLD
-        )
-        if len(boxes) ==0:
+        with torch.no_grad():
+            boxes, logits, phrases = predict(
+                model=model,
+                image=image,
+                caption=TEXT_PROMPT,
+                box_threshold=BOX_THRESHOLD,
+                text_threshold=TEXT_THRESHOLD,
+            )
+        if len(boxes) == 0:
             continue
         # save license plate images to separate folder
         h, w, _ = image_source.shape
         boxes = boxes * torch.Tensor([w, h, w, h])
         xyxy = box_convert(boxes=boxes, in_fmt="cxcywh", out_fmt="xyxy").numpy()
         detections = sv.Detections(xyxy=xyxy)
-        image_source=cv2.cvtColor(image_source, cv2.COLOR_BGR2RGB)
+        image_source = cv2.cvtColor(image_source, cv2.COLOR_BGR2RGB)
         for i in range(len(detections)):
             x1, y1, x2, y2 = detections.xyxy[i].astype(int)
             license_plate_file_name = f"{os.path.splitext(file_name)[0]}_{i}.jpg"
-            license_plate_file_path = os.path.join(LICENSE_PLATE_FOLDER, license_plate_file_name)
-            cv2.imwrite(license_plate_file_path,image_source[y1:y2,x1:x2])
+            license_plate_file_path = os.path.join(
+                LICENSE_PLATE_FOLDER, license_plate_file_name
+            )
+            cv2.imwrite(license_plate_file_path, image_source[y1:y2, x1:x2])
 
 
 # %%
--- Vehicle_detection.py
+++ Vehicle_detection.py
@@ -1,83 +1,84 @@
 import cv2
 from ultralytics import YOLO
 import supervision as sv
-import numpy as np
+import subprocess
 
 import os
-os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-#videopath="assets\\CCTV footage.mp4"
-#videopath="assets\\main gate 2.mkv"
-#videopath="assets\\CCTV Main Gate.mp4"
-#videopath="assets\\Road traffic video for object recognition.mp4"
-#videopath="assets\\main gate 1.mkv"
-#videopath="assets\\SimTrim_Trim.mp4"
-#videopath="assets\\vid2.mp4"
-videopath="assets\\video3.mp4"
-outdir="output/"
-vi=sv.VideoInfo.from_video_path(videopath)
-tracked=set()
-LINE_END = sv.Point(0, vi.height//2+150)
-LINE_START = sv.Point(vi.width, vi.height//2+150)
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+# videopath="assets\\CCTV footage.mp4"
+# videopath="assets\\main gate 2.mkv
+# videopath="assets\\CCTV Main Gate.mp4"
+# videopath="assets\\CCTV Main Gate long.mp4"
+# videopath="assets\\LPD.mp4"
+# videopath="assets\\Road traffic video for object recognition.mp4"
+# videopath="assets\\main gate 1.mkv"
+# videopath="assets\\SimTrim_Trim.mp4"
+# videopath="assets\\vid2.mp4"
+videopath = "assets\\video3.mp4"
+outdir = "output/"
+vi = sv.VideoInfo.from_video_path(videopath)
+tracked = set()
+LINE_END = sv.Point(0, vi.height // 2 + 150)
+LINE_START = sv.Point(vi.width, vi.height // 2 + 150)
+
 
 def main():
     line_counter = sv.LineZone(start=LINE_START, end=LINE_END)
     line_annotator = sv.LineZoneAnnotator(thickness=2, text_thickness=1, text_scale=0.5)
-    box_annotator = sv.BoxAnnotator(
-        thickness=2,
-        text_thickness=1,
-        text_scale=0.5
-    )
+    box_annotator = sv.BoxAnnotator(thickness=2, text_thickness=1, text_scale=0.5)
     model = YOLO("yolov8n.pt")
-    for result in model.track(source=videopath, stream=True,device=0):
-        
+    for result in model.track(source=videopath, stream=True, device=0):
+
         frame = result.orig_img
         detections = sv.Detections.from_yolov8(result)
 
         if result.boxes.id is not None:
             detections.tracker_id = result.boxes.id.cpu().numpy().astype(int)
-        
-        detections = detections[(detections.class_id == 7)|(detections.class_id == 2)|(detections.class_id == 3)]
+
+        detections = detections[
+            (detections.class_id == 7)
+            | (detections.class_id == 2)
+            | (detections.class_id == 3)
+        ]
         labels = [
             f"{tracker_id} {model.model.names[class_id]} {confidence:0.2f}"
-            for __, _, confidence, class_id, tracker_id
-            in detections
+            for __, _, confidence, class_id, tracker_id in detections
         ]
-
-        for box,cls_id in zip(result.boxes,detections.class_id):
-            if box.cls.cpu().numpy().astype(int) in [7,2,3]:   
-                if box is not None:    
-                    tracker_id=box.id.cpu().numpy().astype(int)
-                x,y,x1,y1=(box.xyxy.numpy().astype(int))[0]
-                if x<0:
-                    x=0
-                if y<0:
-                    y=0
-                img_name=str(model.names[cls_id])+"_"+str(tracker_id)+".jpg"
-                path=outdir+img_name
-                if not os.path.exists(path):
-                    cv2.imwrite(path,frame[y:y1,x:x1])
-                else:
-                    img_old=cv2.imread(path)
-                    #area_old=img_old.shape[0]
-                    #area_new=(y1-y)
-                    area_old=img_old.shape[1]
-                    area_new=(x1-x)
-                    if area_new>=area_old:
-                        cv2.imwrite(path,frame[y:y1,x:x1])
-"""         cv2.imshow("yolov8", np.zeros((720,720)))
-        if (cv2.waitKey(1) == 27):
-            break
+        for box, cls_id in zip(result.boxes, detections.class_id):
+            if box.cls.cpu().numpy().astype(int) in [7, 2, 3]:
+                if box.id is not None:
+                    tracker_id = box.id.cpu().numpy().astype(int)
+                    x, y, x1, y1 = (box.xyxy.numpy().astype(int))[0]
+                    if x < 0:
+                        x = 0
+                    if y < 0:
+                        y = 0
+                    img_name = str(model.names[cls_id]) + "_" + str(tracker_id) + ".jpg"
+                    path = outdir + img_name
+                    if not os.path.exists(path):
+                        cv2.imwrite(path, frame[y:y1, x:x1])
+                    else:
+                        img_old = cv2.imread(path)
+                        area_old = img_old.shape[0]
+                        area_new = y1 - y
+                        # area_old=img_old.shape[1]
+                        # area_new=(x1-x)
+                        if area_new >= area_old:
+                            cv2.imwrite(path, frame[y:y1, x:x1])
+        box_annotator.annotate(frame, detections)
         frame = box_annotator.annotate(
-            scene=frame, 
-            detections=detections,
-            labels=labels
+            scene=frame, detections=detections, labels=labels
         )
         line_counter.trigger(detections=detections)
-        line_annotator.annotate(frame=frame, line_counter=line_counter)"""
+        line_annotator.annotate(frame=frame, line_counter=line_counter)
+        cv2.imshow("yolov8", frame)
+        if cv2.waitKey(1) == 27:
+            break
+
+
 if __name__ == "__main__":
     main()
-    import subprocess
-
-    # Run a Python file
+    cv2.destroyAllWindows()
     subprocess.run(["python", "License_Plate_detection.py"])
